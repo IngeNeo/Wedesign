8d4d19e82cd30e0dfac2aadba33bffe58e1e94e7
Roger Valencia <roger.valencia.garcia@hotmail.com>
2022-02-02 17:16:37 -0500

    Programando menu movil con cierre

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 // Mobile Menu
 
+document.getElementById("hamburguer-icon").onclick = function () {
+	document.getElementById("sliding-header-menu-outer").style.right = "0";
+};
+
+document.getElementById("sliding-header-menu-close-button").onclick =
+	function () {
+		document.getElementById("sliding-header-menu-outer").style.right = "-320px";
+	};
+
 // About us Tab
 
 var aboutUs = {
